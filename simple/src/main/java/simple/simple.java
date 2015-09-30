ae7f9c3ab030e1d45b316c9589b4c5e7632dbea5
Tanja Kuery <Tanja Kuery>
2015-09-30 23:02:49 +0200

    jeah

--- simple/src/main/java/simple/simple.java
+++ simple/src/main/java/simple/simple.java
@@ -2,20 +2,18 @@ package simple;
 
 import jrtr.*;
 import jrtr.glrenderer.*;
-import jrtr.gldeferredrenderer.*;
-
 import javax.swing.*;
 import java.awt.event.*;
-import javax.vecmath.*;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.vecmath.*;
 
 /**
  * Implements a simple application that opens a 3D rendering window and 
  * shows a rotating cube.
  */
-public class simple
-{	
+public class simple{	
 	static RenderPanel renderPanel;
 	static RenderContext renderContext;
 	static Shader normalShader;
@@ -24,6 +22,9 @@ public class simple
 	static SimpleSceneManager sceneManager;
 	static Shape shape;
 	static float currentstep, basicstep;
+	static VertexData vertexData;
+	static String whatShape;
+	
 
 	/**
 	 * An extension of {@link GLRenderPanel} or {@link SWRenderPanel} to 
@@ -39,91 +40,41 @@ public class simple
 		 */
 		public void init(RenderContext r)
 		{
-			renderContext = r;
+			simple.renderContext = r;
+			simple.vertexData = make(simple.whatShape);
 			
-			// Make a simple geometric object: a cube
-			
-			// The vertex positions of the cube
-			float v[] = {-1,-1,1, 1,-1,1, 1,1,1, -1,1,1,		// front face
-				         -1,-1,-1, -1,-1,1, -1,1,1, -1,1,-1,	// left face
-					  	 1,-1,-1,-1,-1,-1, -1,1,-1, 1,1,-1,		// back face
-						 1,-1,1, 1,-1,-1, 1,1,-1, 1,1,1,		// right face
-						 1,1,1, 1,1,-1, -1,1,-1, -1,1,1,		// top face
-						-1,-1,1, -1,-1,-1, 1,-1,-1, 1,-1,1};	// bottom face
-
-			// The vertex normals 
-			float n[] = {0,0,1, 0,0,1, 0,0,1, 0,0,1,			// front face
-				         -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
-					  	 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
-						 1,0,0, 1,0,0, 1,0,0, 1,0,0,			// right face
-						 0,1,0, 0,1,0, 0,1,0, 0,1,0,			// top face
-						 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0};		// bottom face
-
-			// The vertex colors
-			float c[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
-					     0,1,0, 0,1,0, 0,1,0, 0,1,0,
-						 1,0,0, 1,0,0, 1,0,0, 1,0,0,
-						 0,1,0, 0,1,0, 0,1,0, 0,1,0,
-						 0,0,1, 0,0,1, 0,0,1, 0,0,1,
-						 0,0,1, 0,0,1, 0,0,1, 0,0,1};
-
-			// Texture coordinates 
-			float uv[] = {0,0, 1,0, 1,1, 0,1,
-					  0,0, 1,0, 1,1, 0,1,
-					  0,0, 1,0, 1,1, 0,1,
-					  0,0, 1,0, 1,1, 0,1,
-					  0,0, 1,0, 1,1, 0,1,
-					  0,0, 1,0, 1,1, 0,1};
-
-			// Construct a data structure that stores the vertices, their
-			// attributes, and the triangle mesh connectivity
-			VertexData vertexData = renderContext.makeVertexData(24);
-			vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
-			vertexData.addElement(v, VertexData.Semantic.POSITION, 3);
-			vertexData.addElement(n, VertexData.Semantic.NORMAL, 3);
-			vertexData.addElement(uv, VertexData.Semantic.TEXCOORD, 2);
-			
-			// The triangles (three vertex indices for each triangle)
-			int indices[] = {0,2,3, 0,1,2,			// front face
-							 4,6,7, 4,5,6,			// left face
-							 8,10,11, 8,9,10,		// back face
-							 12,14,15, 12,13,14,	// right face
-							 16,18,19, 16,17,18,	// top face
-							 20,22,23, 20,21,22};	// bottom face
-
-			vertexData.addIndices(indices);
 								
 			// Make a scene manager and add the object
-			sceneManager = new SimpleSceneManager();
-			shape = new Shape(vertexData);
-			sceneManager.addShape(shape);
+			simple.sceneManager = new SimpleSceneManager();
+			simple.shape = new Shape(simple.vertexData);
+			simple.sceneManager.addShape(simple.shape);
 
 			// Add the scene to the renderer
-			renderContext.setSceneManager(sceneManager);
+			simple.renderContext.setSceneManager(simple.sceneManager);
 			
 			// Load some more shaders
-		    normalShader = renderContext.makeShader();
+		    simple.normalShader = simple.renderContext.makeShader();
 		    try {
-		    	normalShader.load("../jrtr/shaders/normal.vert", "../jrtr/shaders/normal.frag");
+		    	simple.normalShader.load("../jrtr/shaders/normal.vert", "../jrtr/shaders/normal.frag");
 		    } catch(Exception e) {
 		    	System.out.print("Problem with shader:\n");
 		    	System.out.print(e.getMessage());
 		    }
 	
-		    diffuseShader = renderContext.makeShader();
+		    simple.diffuseShader = simple.renderContext.makeShader();
 		    try {
-		    	diffuseShader.load("../jrtr/shaders/diffuse.vert", "../jrtr/shaders/diffuse.frag");
+		    	simple.diffuseShader.load("../jrtr/shaders/diffuse.vert", "../jrtr/shaders/diffuse.frag");
 		    } catch(Exception e) {
 		    	System.out.print("Problem with shader:\n");
 		    	System.out.print(e.getMessage());
 		    }
 
 		    // Make a material that can be used for shading
-			material = new Material();
-			material.shader = diffuseShader;
-			material.diffuseMap = renderContext.makeTexture();
+			simple.material = new Material();
+			simple.material.shader = simple.diffuseShader;
+			simple.material.diffuseMap = simple.renderContext.makeTexture();
 			try {
-				material.diffuseMap.load("../textures/plant.jpg");
+				simple.material.diffuseMap.load("../textures/plant.jpg");
 			} catch(Exception e) {				
 				System.out.print("Could not load texture.\n");
 				System.out.print(e.getMessage());
@@ -131,32 +82,33 @@ public class simple
 
 			// Register a timer task
 		    Timer timer = new Timer();
-		    basicstep = 0.01f;
-		    currentstep = basicstep;
+		    simple.basicstep = 0.01f;
+		    simple.currentstep = simple.basicstep;
 		    timer.scheduleAtFixedRate(new AnimationTask(), 0, 10);
 		}
-	}
+
+		
 
 	/**
 	 * A timer task that generates an animation. This task triggers
 	 * the redrawing of the 3D scene every time it is executed.
 	 */
-	public static class AnimationTask extends TimerTask
+	public class AnimationTask extends TimerTask
 	{
 		public void run()
 		{
 			// Update transformation by rotating with angle "currentstep"
-    		Matrix4f t = shape.getTransformation();
+    		Matrix4f t = simple.shape.getTransformation();
     		Matrix4f rotX = new Matrix4f();
-    		rotX.rotX(currentstep);
+    		rotX.rotX(simple.currentstep);
     		Matrix4f rotY = new Matrix4f();
-    		rotY.rotY(currentstep);
+    		rotY.rotY(simple.currentstep);
     		t.mul(rotX);
     		t.mul(rotY);
-    		shape.setTransformation(t);
+    		simple.shape.setTransformation(t);
     		
     		// Trigger redrawing of the render window
-    		renderPanel.getCanvas().repaint(); 
+    		simple.renderPanel.getCanvas().repaint(); 
 		}
 	}
 
@@ -192,51 +144,51 @@ public class simple
 			{
 				case 's': {
 					// Stop animation
-					currentstep = 0;
+					simple.currentstep = 0;
 					break;
 				}
 				case 'p': {
 					// Resume animation
-					currentstep = basicstep;
+					simple.currentstep = simple.basicstep;
 					break;
 				}
 				case '+': {
 					// Accelerate roation
-					currentstep += basicstep;
+					simple.currentstep += simple.basicstep;
 					break;
 				}
 				case '-': {
 					// Slow down rotation
-					currentstep -= basicstep;
+					simple.currentstep -= simple.basicstep;
 					break;
 				}
 				case 'n': {
 					// Remove material from shape, and set "normal" shader
-					shape.setMaterial(null);
-					renderContext.useShader(normalShader);
+					simple.shape.setMaterial(null);
+					simple.renderContext.useShader(simple.normalShader);
 					break;
 				}
 				case 'd': {
 					// Remove material from shape, and set "default" shader
-					shape.setMaterial(null);
-					renderContext.useDefaultShader();
+					simple.shape.setMaterial(null);
+					simple.renderContext.useDefaultShader();
 					break;
 				}
 				case 'm': {
 					// Set a material for more complex shading of the shape
-					if(shape.getMaterial() == null) {
-						shape.setMaterial(material);
+					if(simple.shape.getMaterial() == null) {
+						simple.shape.setMaterial(simple.material);
 					} else
 					{
-						shape.setMaterial(null);
-						renderContext.useDefaultShader();
+						simple.shape.setMaterial(null);
+						simple.renderContext.useDefaultShader();
 					}
 					break;
 				}
 			}
 			
 			// Trigger redrawing
-			renderPanel.getCanvas().repaint();
+			simple.renderPanel.getCanvas().repaint();
 		}
 		
 		public void keyReleased(KeyEvent e)
@@ -255,24 +207,100 @@ public class simple
 	 * for initialization automatically. It then constructs a simple 3D scene, 
 	 * and starts a timer task to generate an animation.
 	 */
-	public static void main(String[] args)
-	{		
+	public static void main(String[] args){		
 		// Make a render panel. The init function of the renderPanel
 		// (see above) will be called back for initialization.
-		renderPanel = new SimpleRenderPanel();
-		
+		simple.renderPanel = new SimpleRenderPanel();
+		simple.whatShape = args[0];
+	
 		// Make the main window of this application and add the renderer to it
 		JFrame jframe = new JFrame("simple");
 		jframe.setSize(500, 500);
 		jframe.setLocationRelativeTo(null); // center of screen
-		jframe.getContentPane().add(renderPanel.getCanvas());// put the canvas into a JFrame window
+		jframe.getContentPane().add(simple.renderPanel.getCanvas());// put the canvas into a JFrame window
 
 		// Add a mouse and key listener
-	    renderPanel.getCanvas().addMouseListener(new SimpleMouseListener());
-	    renderPanel.getCanvas().addKeyListener(new SimpleKeyListener());
-		renderPanel.getCanvas().setFocusable(true);   	    	    
+	    simple.renderPanel.getCanvas().addMouseListener(new SimpleMouseListener());
+	    simple.renderPanel.getCanvas().addKeyListener(new SimpleKeyListener());
+		simple.renderPanel.getCanvas().setFocusable(true);   	    	    
 	    
 	    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    jframe.setVisible(true); // show window
+	}
+
+	private VertexData make(String string) {
+		
+		
+		String theShape = string;
+	
+		switch(theShape){
+			case "cube": return makeCube();
+			case "cylinder": return makeCylinder();
+		}
+		
+		return null;
+	}
+
+	private VertexData makeCylinder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public VertexData makeCube() {
+		// Make a simple geometric object: a cube
+
+		// The vertex positions of the cube
+		float v[] = {-1,-1,1, 1,-1,1, 1,1,1, -1,1,1,		// front face
+			         -1,-1,-1, -1,-1,1, -1,1,1, -1,1,-1,	// left face
+				  	 1,-1,-1,-1,-1,-1, -1,1,-1, 1,1,-1,		// back face
+					 1,-1,1, 1,-1,-1, 1,1,-1, 1,1,1,		// right face
+					 1,1,1, 1,1,-1, -1,1,-1, -1,1,1,		// top face
+					-1,-1,1, -1,-1,-1, 1,-1,-1, 1,-1,1};	// bottom face
+
+		// The vertex normals 
+		float n[] = {0,0,1, 0,0,1, 0,0,1, 0,0,1,			// front face
+			         -1,0,0, -1,0,0, -1,0,0, -1,0,0,		// left face
+				  	 0,0,-1, 0,0,-1, 0,0,-1, 0,0,-1,		// back face
+					 1,0,0, 1,0,0, 1,0,0, 1,0,0,			// right face
+					 0,1,0, 0,1,0, 0,1,0, 0,1,0,			// top face
+					 0,-1,0, 0,-1,0, 0,-1,0, 0,-1,0};		// bottom face
+
+		// The vertex colors
+		float c[] = {1,0,0, 1,0,0, 1,0,0, 1,0,0,
+				     0,1,0, 0,1,0, 0,1,0, 0,1,0,
+					 1,0,0, 1,0,0, 1,0,0, 1,0,0,
+					 0,1,0, 0,1,0, 0,1,0, 0,1,0,
+					 0,0,1, 0,0,1, 0,0,1, 0,0,1,
+					 0,0,1, 0,0,1, 0,0,1, 0,0,1};
+
+		// Texture coordinates 
+		float uv[] = {0,0, 1,0, 1,1, 0,1,
+				  0,0, 1,0, 1,1, 0,1,
+				  0,0, 1,0, 1,1, 0,1,
+				  0,0, 1,0, 1,1, 0,1,
+				  0,0, 1,0, 1,1, 0,1,
+				  0,0, 1,0, 1,1, 0,1};
+
+		// Construct a data structure that stores the vertices, their
+		// attributes, and the triangle mesh connectivity
+		VertexData vertexData = simple.renderContext.makeVertexData(24);
+		vertexData.addElement(c, VertexData.Semantic.COLOR, 3);
+		vertexData.addElement(v, VertexData.Semantic.POSITION, 3);
+		vertexData.addElement(n, VertexData.Semantic.NORMAL, 3);
+		vertexData.addElement(uv, VertexData.Semantic.TEXCOORD, 2);
+		
+		// The triangles (three vertex indices for each triangle)
+		int indices[] = {0,2,3, 0,1,2,			// front face
+						 4,6,7, 4,5,6,			// left face
+						 8,10,11, 8,9,10,		// back face
+						 12,14,15, 12,13,14,	// right face
+						 16,18,19, 16,17,18,	// top face
+						 20,22,23, 20,21,22};	// bottom face
+
+		vertexData.addIndices(indices);
+		
+		return vertexData;
+		
+	}
 	}
 }
